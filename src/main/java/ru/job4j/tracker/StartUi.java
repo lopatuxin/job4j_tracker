84b0574562c48c62708e51bb4f72a2a432956f96
Антон <lopatuxin.a@gmail.com>
2022-04-14 13:00:20 +0300

    2.3. Реализация класса StartUI. Добавление заявки [#500744 #288729]

--- src/main/java/ru/job4j/tracker/StartUi.java
+++ src/main/java/ru/job4j/tracker/StartUi.java
@@ -17,9 +17,7 @@ public class StartUi {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
-            } else if (select != 6) {
-                System.out.println("Пользователь выбрал: " + select);
-            } else {
+            } else if (select == 6) {
                 run = false;
             }
         }
