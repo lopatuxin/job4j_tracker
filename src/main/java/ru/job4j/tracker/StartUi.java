5aaf5db7acf52beff71343e73617e25fff6cca91
Антон <lopatuxin.a@gmail.com>
2022-04-16 08:37:46 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#181778 #289482]

--- src/main/java/ru/job4j/tracker/StartUi.java
+++ src/main/java/ru/job4j/tracker/StartUi.java
@@ -72,7 +72,7 @@ public class StartUi {
         boolean run = true;
         while (run) {
             showMenu();
-            int select = Integer.parseInt(input.askStr("Select : "));
+            int select = input.askInt("Select : ");
             if (select == 0) {
                 createItem(input, tracker);
             } else if (select == 1) {
--- src/test/java/ru/job4j/tracker/StartUiTest.java
+++ src/test/java/ru/job4j/tracker/StartUiTest.java
@@ -18,4 +18,29 @@ public class StartUiTest {
         assertThat(created.getName(), is(expected.getName()));
     }
 
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
+                "edited item"
+        };
+        StartUi.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("edited item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        String[] answers = {"Test"};
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(answers);
+        StartUi.createItem(input, tracker);
+        StartUi.deleteItem(input, tracker);
+        Item deleted = tracker.findAll()[0];
+        Item expected = null;
+        assertThat(deleted, is(expected));
+    }
 }
