7c59c8cb914c603018b0f2a602aad497e022e70c
Антон <lopatuxin.a@gmail.com>
2022-04-17 22:47:39 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#181778 #289482]

--- src/test/java/ru/job4j/tracker/StartUiTest.java
+++ src/test/java/ru/job4j/tracker/StartUiTest.java
@@ -39,7 +39,6 @@ public class StartUiTest {
         tracker.add(item);
         String[] answers = {String.valueOf(item.getId())};
         StartUi.deleteItem(new StubInput(answers), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertNull(deleted);
+        assertNull(tracker.findById(item.getId()));
     }
 }
