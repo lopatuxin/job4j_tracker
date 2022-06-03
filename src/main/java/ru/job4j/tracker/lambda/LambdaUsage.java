cf34932a7040505f02a7fc1ad299ae28646e2431
Антон <lopatuxin.a@gmail.com>
2022-06-03 14:49:02 +0300

    2.2. Лямбда блок [#249208]

--- src/main/java/ru/job4j/tracker/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/tracker/lambda/LambdaUsage.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public class LambdaUsage {
+
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("eeeee", "a",  "ccc", "dddd", "bb");
         Comparator<String> comparator = (left, right) -> {
