863979bc0058df0c111c99cff60601d15aa23e63
Антон <lopatuxin.a@gmail.com>
2022-06-05 21:25:32 +0300

    5. Подсчет функции в диапазоне. [#24251]

---
+++ src/test/java/ru/job4j/tracker/lambda/CountingFunctionTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.tracker.lambda;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+public class CountingFunctionTest {
+
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        List<Double> result = CountingFunction.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+    @Test
+    public  void whenLinearFunctionThenLinerResult() {
+        List<Double> result = CountingFunction.diapason(5, 8, x -> x);
+        List<Double> expected = Arrays.asList(5D, 6D, 7D);
+        assertThat(result, is(expected));
+    }
+    @Test
+    public  void whenQuadraticFunctionThenQuadraticResult() {
+        List<Double> result = CountingFunction.diapason(5, 8, x -> x * x);
+        List<Double> expected = Arrays.asList(25D, 36D, 49D);
+        assertThat(result, is(expected));
+    }
+    @Test
+    public  void whenIndicativeFunctionThenIndicativeResult() {
+        List<Double> result = CountingFunction.diapason(5, 8, x -> 2 * x);
+        List<Double> expected = Arrays.asList(10D, 12D, 14D);
+        assertThat(result, is(expected));
+    }
+}
