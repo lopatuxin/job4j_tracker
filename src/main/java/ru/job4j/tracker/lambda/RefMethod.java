83b0e566b744fc1465959fd992f068e0931a1bbd
Антон <lopatuxin.a@gmail.com>
2022-06-05 08:20:22 +0300

    3. Ссылки на методы [#70608 #309022]

--- src/main/java/ru/job4j/tracker/lambda/RefMethod.java
+++ src/main/java/ru/job4j/tracker/lambda/RefMethod.java
@@ -7,12 +7,13 @@ import java.util.function.Consumer;
 public class RefMethod {
     public static void main(String[] args) {
         List<String> names = Arrays.asList(
-                "Ivan"
-                ,"Anton"
+                "Ivan",
+                "Anton"
         );
         Consumer<String> out = RefMethod::cutOut;
         names.forEach(out);
     }
+
     public static void cutOut(String value) {
         if (value.length() > 10) {
             System.out.println(value.substring(0, 10) + "..");
