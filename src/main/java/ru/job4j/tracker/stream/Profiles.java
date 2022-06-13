b42c4da2c29a390682c15b802edd7f3d8a66ebf7
Антон <lopatuxin.a@gmail.com>
2022-06-13 22:40:19 +0300

    2. Список адресов [#110062 #312085]

--- src/main/java/ru/job4j/tracker/stream/Profiles.java
+++ src/main/java/ru/job4j/tracker/stream/Profiles.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).toList();
+        return profiles.stream()
+                .map(profile -> profile.getAddress())
+                .toList();
     }
 }
