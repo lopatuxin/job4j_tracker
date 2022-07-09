c709b0c2f4fa3a9debccdeac013b9864871bf4d4
Антон <lopatuxin.a@gmail.com>
2022-07-09 14:36:49 +0300

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/tracker/stream/Tuple.java
@@ -0,0 +1,44 @@
+package ru.job4j.tracker.stream;
+
+import java.util.Objects;
+
+public class Tuple {
+    private String name;
+    private double score;
+
+    public Tuple(String name, double score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Tuple tuple = (Tuple) o;
+
+        if (Double.compare(tuple.score, score) != 0) {
+            return false;
+        }
+        return Objects.equals(name, tuple.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        temp = Double.doubleToLongBits(score);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
