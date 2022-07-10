bddc4a6d6db251f05fbc8501dbb4a35754b27302
Антон <lopatuxin.a@gmail.com>
2022-07-10 20:27:11 +0300

    2. Аттестация [#49777]

--- src/main/java/ru/job4j/tracker/stream/Analyze.java
+++ src/main/java/ru/job4j/tracker/stream/Analyze.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker.stream;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
